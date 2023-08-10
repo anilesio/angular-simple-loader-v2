73d39131f6f4174712d002a432cbbf9bca418a4b
anilesio <anilesio08@gmail.com>
2023-08-10 15:28:17 +0100

    feat: init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoadingComponent } from './components/loading/loading.component';
 import { SimpleLoadingComponent } from './components/simple-loading/simple-loading.component';
 
 @NgModule({
   declarations: [
-    LoadingComponent,
     SimpleLoadingComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LoadingComponent,
     SimpleLoadingComponent
   ]
 })
